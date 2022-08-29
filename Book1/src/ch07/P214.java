97d578c356e7403aa234a760290bbc5c104cb11f
kcy013100 <kcy013100@naver.com>
2022-08-29 13:50:21 +0900

    2022/08/29 추가

--- Book1/src/ch07/P214.java
+++ Book1/src/ch07/P214.java
@@ -1,9 +1,9 @@
 package ch07;
 
-public class P213 {
+public class P214 {
 	public static void main(String[] args) {
-		Book[] bookArray1 = new Book[3];
-		Book[] bookArray2 = new Book[3];
+		Book[ ] bookArray1 = new Book[3];
+		Book[ ] bookArray2 = new Book[3];
 		
 		bookArray1[0] = new Book("태백산맥", "조정래");
 		bookArray1[1] = new Book("데미안", "헤르만 헤세");
@@ -14,7 +14,18 @@ public class P213 {
 			bookArray2[i].showBookInfo();
 		}
 		
+		System.out.println("=== bookArray1 ===");
+		bookArray1[0].setBookName("나목");
+		bookArray1[0].setAuthor("박완서");
 		
+		System.out.println("=== bookArray2 ===");
+		for(int i=0; i < bookArray2.length; i++) {
+			bookArray2[i].showBookInfo();
+		}
 		
 	}
-}
+
+		
+	}
+
+
