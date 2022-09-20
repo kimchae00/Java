356e77010cf2a8d51cb168d314d2fd7a082da425
kcy013100 <kcy013100@naver.com>
2022-09-20 13:49:37 +0900

    22/09/20

--- Backjoon/src/step03/_12.java
+++ Backjoon/src/step03/_12.java
@@ -1,5 +1,26 @@
 package step03;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/09/19
+ * 이름 : 김채영
+ * 내용 : 각 테스트 케이스마다 A+B를 출력
+ */
 public class _12 {
+	public static void main(String[] args) {
+		
+Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int a, b;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			
+			System.out.println(a+b);
+		}
+		sc.close();
+	}
 
 }
--- Backjoon/src/step03/_13.java
+++ Backjoon/src/step03/_13.java
@@ -1,5 +1,21 @@
 package step03;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/09/19
+ * 이름 : 김채영
+ * 내용 : 첫째 줄에 N의 사이클 길이를 출력
+ */
 public class _13 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(a*0.1 > ) {
+			
+		}
+	}
 
 }
