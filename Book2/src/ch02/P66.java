e4707a5fcf8a243bcb2cf029aec72a895c7375a8
kcy013100 <kcy013100@naver.com>
2022-09-26 13:43:43 +0900

    22/09/26

---
+++ Book2/src/ch02/P66.java
@@ -0,0 +1,17 @@
+package ch02;
+
+public class P66 {
+	public static void main(String[] args) {
+		// float의 정밀도(대략 소수 7자리)
+		float f1 = 1.0000001f;
+		System.out.println(f1);
+		float f2 = 1.00000001f;
+		System.out.println(f2);
+		
+		// double의 정밀도(대략 소수 15자리)
+		double d1 = 1.000000000000001;
+		System.out.println(d1);
+		double d2 = 1.0000000000000001;
+		System.out.println(d2);
+	}
+}
---
+++ Book2/src/ch02/P74.java
@@ -0,0 +1,22 @@
+package ch02;
+
+public class P74 {
+	public static void main(String[] args) {
+		
+		// 캐스팅 방법 1: 자료형
+		int value1 = (int) 5.3;
+		long value2 = (long) 10;
+		float value3 = (float) 5.8;
+		double value4 = (double) 16;
+		
+		System.out.println(value1);
+		System.out.println(value2);
+		System.out.println(value3);
+		System.out.println(value4);
+		System.out.println();
+		
+		// 캐스팅 방법 2: L, F
+		
+	}
+
+}
