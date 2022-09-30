5f158ce685651676d78b93178c66a05a060f65b3
kcy013100 <kcy013100@naver.com>
2022-09-30 13:45:31 +0900

    22/09/30

--- Backjoon/src/step04/_01.java
+++ Backjoon/src/step04/_01.java
@@ -1,10 +1,29 @@
 package step04;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2022/09/29
  * 이름 : 김채영
  * 내용 : 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력
  */
 public class _01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int a = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
 
 }
