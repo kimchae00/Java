cc61a0b671690074feec5cba15a1a7fb857e763c
kcy013100 <kcy013100@naver.com>
2022-10-04 13:42:39 +0900

    22/10/04

--- Backjoon/src/step04/_01.java
+++ Backjoon/src/step04/_01.java
@@ -1,5 +1,6 @@
 package step04;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -13,17 +14,15 @@ public class _01 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int a = sc.nextInt();
+		int[] arr = new int[n];
 		
-		for(int i=1; i<=n; i++) {
-			
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
 		}
 		
-		
-		
-		
-		
-		
+		sc.close();
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[n-1]);
 	}
 
 }
