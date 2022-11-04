8db053491c03108eba20699c1253c135542bc100
kcy013100 <kcy013100@naver.com>
2022-11-04 13:03:47 +0900

    22/1104 git commit -m 22/11/04 git commit -m 22/1104

--- Backjoon/src/step04/_05.java
+++ Backjoon/src/step04/_05.java
@@ -1,5 +1,27 @@
 package step04;
 
-public class _05 {
+import java.util.Scanner;
 
+/*
+ * 날짜 : 2022/11/04
+ * 이름 : 김채영
+ * 내용 : 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성
+ */
+public class _05 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int[] stud  = new int[31];
+		
+		for(int i=1; i<=28; i++) {
+			int success = sc.nextInt();
+			stud[success] = 1;
+		}
+		
+		for(int i=1; i<stud.length; i++) {
+			if(stud[i] != 1)
+				System.out.println(i);
+		}
+		sc.close();
+	}
 }
--- Backjoon/src/step04/_06.java
+++ Backjoon/src/step04/_06.java
@@ -1,10 +1,26 @@
 package step04;
 
+import java.util.Scanner;
+
 /*
- * 날짜 : 2022/10/05
+ * 날짜 : 2022/11/04
  * 이름 : 김채영
  * 내용 : 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력
  */
 public class _06 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num[] = new int[11];
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+	}
 
 }
